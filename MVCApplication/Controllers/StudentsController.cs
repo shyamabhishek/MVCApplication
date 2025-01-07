22c1cbc3c476e34b3fca60bcea85bfdf313f4617
shyamabhishek <shyamabhishekkumar@gmail.com>
2025-01-07 22:55:44 +0530

    Add project files.

---
+++ MVCApplication/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public string GetMyString()
+        {
+            return "This is my string method called ";
+        }
+        public string GetMyStringWithParameter(int id, string name)
+        {
+            return "This is my ID : " + id + " "+ "name :  " + name;
+        }
+        public ActionResult MyView()
+        {
+            return View("MyFirstView");
+        }public ActionResult MySecondMethod()
+        {
+            return View();
+        }
+    }
+}
