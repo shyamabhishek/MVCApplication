722c90d51baa5cc2c6dd25b21e50138e859aa33b
shyamabhishek <shyamabhishekkumar@gmail.com>
2025-01-10 20:06:12 +0530

    updated 10th jan 2025

---
+++ MVCApplication/Controllers/HelperDemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class HelperDemoController : Controller
+    {
+        // GET: HelperDemo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact(string name=null)
+        {
+            return View();
+        }
+    }
+}
