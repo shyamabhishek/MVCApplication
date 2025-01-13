00d1fa3549b50683a3026d13ff0d034e2901ee7c
shyamabhishek <shyamabhishekkumar@gmail.com>
2025-01-13 19:16:40 +0530

    updated 13th jan 2025

---
+++ MVCApplication/Controllers/TempDataDemoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class TempDataDemoController : Controller
+    {
+        // GET: TempDataDemo
+        public ActionResult Index()
+        {
+            TempData["myKey"] = "This is my action method";
+            return View();
+        }
+        public ActionResult About()
+        {
+            //var tempDataval = TempData["myKey"];
+            var tempDataval = TempData.Peek("myKey");
+            //Session.Abandon();
+            TempData.Keep("myKey");
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            var tempDataval = TempData["myKey"];
+            return View();
+        }
+    }
+}
